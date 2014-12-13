baaf997ca0dbc4292862f744785607a48e713973
Angelo Cisneros <a.e.cisneros@gmail.com>
2014-12-13 14:15:35 -0800

    add method-override

--- app.js
+++ app.js
@@ -16,12 +16,13 @@
  *******************************************************************************
 *******************************************************************************/
 
-var express    = require('express'),
-    bodyParser = require('body-parser'),
-    db         = require('./models'),
-    passport   = require('passport'),
-    session    = require('cookie-session'),
-    app        = express();
+var express        = require('express'),
+    bodyParser     = require('body-parser'),
+    db             = require('./models'),
+    passport       = require('passport'),
+    session        = require('cookie-session'),
+    methodOverride = require('method-override'),
+    app            = express();
 
 /*******************************************************************************
  *******************************************************************************
@@ -31,8 +32,10 @@ var express    = require('express'),
  *******************************************************************************
 *******************************************************************************/
 // set default view engine to EJS
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded( { extended:true } ));
+app.set( 'view engine', 'ejs');
+app.use( express.static(__dirname + '/public'));
+app.use( bodyParser.urlencoded( { extended:true } ));
+app.use( methodOverride('_method'));
 
 
 /*******************************************************************************
